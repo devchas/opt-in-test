12ec82793802b2d889d6e1931cf14f8278b31d27
devchas <devchas@gmail.com>
2016-07-27 15:46:03 -0400

    custom changes

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-exports.url = 'http://localhost:3090';
+exports.url = 'opt-in-test.herokuapp.com';
 exports.senderEmail = "sender@example.com";
 exports.senderName = "Example Sender";
 exports.listID = 348282;
